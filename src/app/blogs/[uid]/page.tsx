761093ac5c6e4158f10c0751caa8a967feeff556
emilio <emilio5139@gmail.com>
2023-09-06 13:48:44 -0400

    first commit

--- src/app/blogs/[uid]/page.tsx
+++ src/app/blogs/[uid]/page.tsx
@@ -11,68 +11,78 @@ type Params = { uid: string };
 export const dynamicParams = false;
 
 export default async function Page({ params }: { params: Params }) {
-    const client = createClient();
-    const page = await client
-        .getByUID("blog_post", params.uid)
-        .catch(() => notFound());
+    // const client = createClient();
+    // const page = await client
+    //     .getByUID("blog_post", params.uid)
+    //     .catch(() => notFound());
 
-    return (
-        <main
-        className="underlineLinks"
-            style={{
-                background: "#111111",
-                display: "flex",
-                justifyContent: 'center',
-            }}
-        >
+        return (
             <div
                 style={{
-                    width: "50rem",
                     display: "flex",
-                    marginTop: '5rem',
-                    flexDirection: 'column',
-                    gap: '2rem',
-                    lineHeight: '200%',
-                    fontWeight: 300,
-                    fontSize: '0.96rem',
-                    color: "#dddddd",
+                    flexDirection: "column",
+                    width: "100vw",
                 }}
-            >
-                <div
-                    style={{
-                        display: "flex",
-                        flexDirection: "row",
-                        fontWeight: 200,
-                        gap: '0.5rem',
-                        fontSize: '0.8rem',
-                        color: "#cccccc",
-                    }}
-                >
-                <div style={{}}>{page.data.author}</div>
-                    <div>•</div>
-                    <>
-                        {page.data.publish_date
-                            ? dateDifference(page.data.publish_date)
-                            : "Invalid Date"}
-                    </>
-                    <div>•</div>
-                    <>
-                        {page.data.read_time == 1
-                            ? "1 min"
-                            : page.data.read_time + " mins"}
-                    </>
-                </div>
-                <div  style={{
-                    fontSize: '2.6rem',
-                    fontWeight: 900,
-                    lineHeight: '130%',
-                    color: "#ffffff",
-                    marginBottom: '3rem',
-                }}>{page.data.title}</div>
-                <SliceZone slices={page.data.slices} components={components} />
-            </div>
-        </main>
-    );
+            />
+        );
+
+    // return (
+    //     <main
+    //     className="underlineLinks"
+    //         style={{
+    //             background: "#111111",
+    //             display: "flex",
+    //             justifyContent: 'center',
+    //         }}
+    //     >
+    //         <div
+    //             style={{
+    //                 width: "50rem",
+    //                 display: "flex",
+    //                 marginTop: '5rem',
+    //                 flexDirection: 'column',
+    //                 gap: '2rem',
+    //                 lineHeight: '200%',
+    //                 fontWeight: 300,
+    //                 fontSize: '0.96rem',
+    //                 color: "#dddddd",
+    //             }}
+    //         >
+    //             <div
+    //                 style={{
+    //                     display: "flex",
+    //                     flexDirection: "row",
+    //                     fontWeight: 200,
+    //                     gap: '0.5rem',
+    //                     fontSize: '0.8rem',
+    //                     color: "#cccccc",
+    //                 }}
+    //             >
+    //             <div style={{}}>{page.data.author}</div>
+    //                 <div>•</div>
+    //                 <>
+    //                     {page.data.publish_date
+    //                         ? dateDifference(page.data.publish_date)
+    //                         : "Invalid Date"}
+    //                 </>
+    //                 <div>•</div>
+    //                 <>
+    //                     {page.data.read_time == 1
+    //                         ? "1 min"
+    //                         : page.data.read_time + " mins"}
+    //                 </>
+    //             </div>
+    //             <div  style={{
+    //                 fontSize: '2.6rem',
+    //                 fontWeight: 900,
+    //                 lineHeight: '130%',
+    //                 color: "#ffffff",
+    //                 marginBottom: '3rem',
+    //             }}>{page.data.title}</div>
+    //             <SliceZone slices={page.data.slices} components={components} />
+    //         </div>
+    //     </main>
+    // );
 }
 
 export async function generateMetadata({
@@ -89,25 +99,12 @@ export async function generateMetadata({
     };
 }
 
-export async function generateStaticParams() {
-    const client = createClient();
-    const pages = await client.getAllByType("blog_post");
+// export async function generateStaticParams() {
+//     const client = createClient();
+//     const pages = await client.getAllByType("blog_post");
 
-    return pages.map((page) => {
-        return { uid: page.uid };
-    });
-}
-
-// export default async function Page() {
-    
-
-//     return (
-//         <div
-//             style={{
-//                 display: "flex",
-//                 flexDirection: "column",
-//                 width: "100vw",
-//             }}
-//         />
-//     );
+//     return pages.map((page) => {
+//         return { uid: page.uid };
+//     });
 // }
+
