39d38dba67b339a8418e9186ef80577cd3078a7f
emilio <emilio5139@gmail.com>
2023-10-17 18:38:06 -0400

    first commit

--- src/prismicio.ts
+++ src/prismicio.ts
@@ -37,7 +37,7 @@ export const createClient = (config: prismicNext.CreateClientConfig = {}) => {
       // process.env.NODE_ENV === "production"
       // ? { next: { tags: ["prismic"] }, cache: "force-cache" }
       // : { next: { revalidate: 5 } },
-      { next: { revalidate: 5, tags: ["prismic"] } },
+      { next: { revalidate: 5 } },
     ...config,
   });
 
