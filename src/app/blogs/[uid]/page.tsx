89999f183d1c164e3a7d053cf86ef9d9f3460be4
emilio <emilio5139@gmail.com>
2023-10-17 22:58:01 -0400

    first commit

--- src/app/blogs/[uid]/page.tsx
+++ src/app/blogs/[uid]/page.tsx
@@ -17,13 +17,13 @@ export const dynamicParams = false;
 
 export default async function Page({ params }: { params: Params }) {
     const client = createClient();
-    // const page = await client
-    //     .getByUID("blog_post", params.uid)
-    //     .catch(() => notFound());
-        const d = await client
-            .getAllByType("blog_post")
-//fis this to that page is possibly undefinded and the page can handle that
-            const page = d.find((page) => page.uid === params.uid);
+    const page = await client
+        .getByUID("blog_post", params.uid)
+        .catch(() => notFound());
+//         const d = await client
+//             .getAllByType("blog_post")
+// //fis this to that page is possibly undefinded and the page can handle that
+//             const page = d.find((page) => page.uid === params.uid);
 
    
 
